fff42cdbf32a557eb899309f3e97e8423805e656
roy <huweicqupt@163.com>
2019-04-23 14:51:17 +0800

    eslint

---
+++ src/assets/deepClone.js
@@ -0,0 +1,20 @@
+/**
+ * @author roy
+ * @date 2019/4/23
+ * @Description: deepClone
+*/
+module.exports = {
+  deepClone:function () {
+    if (!data || !(data instanceof Object) || typeof data === "function") {
+      return data;
+    }
+    var constructor = data.constructor;
+    var result = new constructor();
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        result[key] = deepClone(data[key]);
+      }
+    }
+    return result;
+  }
+}
