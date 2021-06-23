804e0fcd4b3c31e488901d7c278923352c66b024
roy <huw@t3go.cn>
2021-06-23 13:23:05 +0800

    Merge branch 'testB' into testC
    # Conflicts:
    #	src/vuex/stores.js

--- src/vuex/stores.js
+++ src/vuex/stores.js
@@ -3,14 +3,6 @@ stores.vuexNum = {
   state: {
     vuexNum: 1
   },
-  mutations: {
-    changeName: function (state) {
-      state.vuexNum++;
-    },
-    changeNameB: function (state) {
-      state.vuexNum++;
-    }
-  },
   // A的修改
   changeNameB: function (state) {
     state.vuexNum++;
