1001ae2e430f2078737de2421f05b52df86849c1
roy <huw@t3go.cn>
2021-06-23 13:20:13 +0800

    b

--- src/vuex/stores.js
+++ src/vuex/stores.js
@@ -6,6 +6,9 @@ stores.vuexNum = {
   mutations: {
     changeName: function (state) {
       state.vuexNum++;
+    },
+    changeNameB: function (state) {
+      state.vuexNum++;
     }
   }
   // b的改动
